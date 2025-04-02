0b27113e6c1801b0436e31bcdc4b6387f4204c6d
amdek <akifiya@example.com>
2025-04-02 11:12:19 +0300

    Indexing, slicing, and reshaping of an array using numpy

--- Matrix_manipulation.py
+++ Matrix_manipulation.py
@@ -9,3 +9,29 @@ print(reshaped)
 arr2 = np.array([1,2,3])
 expanded = arr2[:, np.newaxis]
 print(expanded)
+
+# Element wise operation
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+print(a + b)
+print(a - b)
+print(a * b)
+print(a/b)
+
+# Mathematical operations
+ar = np.array([4,16,25])
+print(np.sqrt(ar))
+print(np.sum(ar))
+print(np.mean(ar))
+print(np.max(ar))
+
+# Array Indexing, Slicing, and Reshaping
+arr3 = np.array([10,20,30,40,50,60])
+print(arr3[2])
+print(arr3[-1])
+
+print(arr3[1:4])
+print(arr3[:3])
+print(arr3[3:])
+
+resshape = arr3.reshape((2,3))
