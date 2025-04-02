d5bed0236f852d5b835dcd4b86489ebec397cf7d
amdek <akifiya@example.com>
2025-04-02 11:45:47 +0300

    Array normalization function

---
+++ Normalize_array.py
@@ -0,0 +1,20 @@
+# This code noralizes array to be in the range of 0 1nd 1
+import numpy as np
+
+def normalize_array(arr):
+    ##  Normalizes a NumPy array by scaling its values between 0 and 1 using min-max normalization.
+    ## This ensures that the minimum value in the array becomes 0 and the maximum value becomes 1.
+    
+    min_value = np.min(arr)
+    max_value = np.max(arr)
+    
+    if min_value == max_value:
+        return np.zeros_like(arr)
+    return (arr - min_value)/(max_value - min_value)
+
+# Usage of normalize function
+data = np.array([10,20,30,40,50])
+normalized_data = normalize_array(data)
+
+print("Original Array:", data)
+print("Normalized Array:", normalized_data)
